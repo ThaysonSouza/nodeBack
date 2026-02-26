e81f56023aa1fce24b31672b299307ae38ac1361
ThaysonSouza <silvathayson777@gmail.com>
2026-02-26 16:38:05 -0300

    arrumando erros na vercel2

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import cors from "cors";
 import handlerRouter from "./routes/router";
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 app.use(handlerRouter);
