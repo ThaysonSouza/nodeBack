40956e7a1d061be06dda38fe5701ad24f124e991
ThaysonSouza <silvathayson777@gmail.com>
2026-03-05 16:33:46 -0300

    atualiza testes de login para incluir resposta JSON e corrige email no teste de criação

--- src/tests/login.test.ts
+++ src/tests/login.test.ts
@@ -1,32 +1,32 @@
 const url_base:string = "https://node-back-rose.vercel.app/api/login";
  
-// test("POST / login = 200", async () => {
-//     const res = await fetch(url_base, {
-//         method: "POST",
-//         headers: { "Content-Type": "application/json" },
-//         body: JSON.stringify({
-//             email: "joao.silva@email.cm",
-//             senha: "senha123"}
-//         )
-//     });
-//     console.log(res);
-//     const json = await res.json()
-//     console.log(json);
-//     expect(res.status).toBe(200);
-// });
+test("POST / login = 200", async () => {
+    const res = await fetch(url_base, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            email: "jeff@email.com",
+            senha: "senha123"}
+        )
+    });
+    console.log(res);
+    const json = await res.json()
+    console.log(json);
+    expect(res.status).toBe(200);
+});
 
 
-// test("POST / login(sem senha) = 400", async () => {
-//     const res = await fetch(url_base, {
-//         method: "POST",
-//         headers: { "Content-Type": "application/json" },
-//         body: JSON.stringify({
-//             email: "joao.silva@email.cm",
-//             senha: ""}
-//         )
-//     });
-//     expect(res.status).toBe(400);
-// });
+test("POST / login(sem senha) = 400", async () => {
+    const res = await fetch(url_base, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            email: "joao.silva@email.cm",
+            senha: ""}
+        )
+    });
+    expect(res.status).toBe(400);
+});
 
 
 test("POST / create = 200", async () => {
@@ -35,7 +35,7 @@ test("POST / create = 200", async () => {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
             nome: "Jeff",
-            email: "jeff@email.com",
+            email: "jeff2@email.com",
             senha: "senha123",
             telefone: "154004-8922",
             cpf:"123456789-01"
