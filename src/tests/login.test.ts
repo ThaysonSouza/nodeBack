77d3b1c6012f2733ad370f699e253aad80b1e015
ThaysonSouza <silvathayson777@gmail.com>
2026-03-05 15:04:29 -0300

    comentar teste de criação de usuário

--- src/tests/login.test.ts
+++ src/tests/login.test.ts
@@ -28,20 +28,20 @@ test("POST / login(sem senha) = 400", async () => {
 });
 
 
-test("POST / create = 200", async () => {
-    const res = await fetch(url_base + "/cadastro" , {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-            nome: "Teste",
-            email: "teste@email.com",
-            senha: "senha123",
-            telefone: "154002-8922",
-            cpf:"123456789-00"
-        })
-    });
-    expect(res.status).toBe(200);
-    const token = await res.json();
-    
-    // console.log(token)
-});
+// test("POST / create = 200", async () => {
+//     const res = await fetch(url_base + "/cadastro" , {
+//         method: "POST",
+//         headers: { "Content-Type": "application/json" },
+//         body: JSON.stringify({
+//             nome: "Teste",
+//             email: "teste@email.com",
+//             senha: "senha123",
+//             telefone: "154002-8922",
+//             cpf:"123456789-00"
+//         })
+//     });
+//     expect(res.status).toBe(200);
+//     const token = await res.json();
+
+//     // console.log(token)
+// });
